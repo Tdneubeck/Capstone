e595d6964c47f0ce72d62f3f26616ba172db68c5
tony <tdnkvc@umsystem.edu>
2024-03-13 22:58:51 -0500

    Tried some different stuff with login still needs to have a way to authenticate and move to home page

--- app/src/main/java/edu/missouri/collegerewards/ui/conditionalnav/LoginFragment.kt
+++ app/src/main/java/edu/missouri/collegerewards/ui/conditionalnav/LoginFragment.kt
@@ -2,57 +2,57 @@ package edu.missouri.collegerewards.ui.conditionalnav
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import edu.missouri.collegerewards.MainActivity
 import edu.missouri.collegerewards.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginFragment : Fragment() {
 
-    private lateinit var auth: FirebaseAuth
-    private lateinit var emailEditText: EditText
-    private lateinit var passwordEditText: EditText
+    private lateinit var username: EditText
+    private lateinit var password: EditText
     private lateinit var loginButton: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_fragment)
+    private lateinit var mAuth: FirebaseAuth
 
-        auth = FirebaseAuth.getInstance()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
 
-        // Find views by their IDs
-        emailEditText = findViewById(R.id.username)
-        passwordEditText = findViewById(R.id.password)
-        loginButton = findViewById(R.id.enter_button)
+        mAuth = FirebaseAuth.getInstance()
 
+        username = view.findViewById(R.id.username)
+        password = view.findViewById(R.id.password)
+        loginButton = view.findViewById(R.id.enter_button)
         loginButton.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
-
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                loginUser(email, password)
-            } else {
-                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
-            }
+            loginUser(username.text.toString(), password.text.toString())
         }
+
+        return view
     }
 
-
-    private fun loginUser(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
+    private fun loginUser(username: String, password: String) {
+        mAuth.signInWithEmailAndPassword(username, password)
+            .addOnCompleteListener(requireActivity(), OnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    // Login success, navigate to next activity or perform necessary action
-                    Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = mAuth.currentUser
+                    // Navigate user to another screen
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
                 } else {
-                    // Login failed
-                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(requireContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
                 }
-            }
+            })
     }
 }
--- app/src/main/java/edu/missouri/collegerewards/ui/conditionalnav/RegisterFragment.kt
+++ app/src/main/java/edu/missouri/collegerewards/ui/conditionalnav/RegisterFragment.kt
@@ -1,74 +1,105 @@
 package edu.missouri.collegerewards.ui.conditionalnav
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.database.FirebaseDatabase
 import edu.missouri.collegerewards.R
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterFragment : Fragment() {
 
-    private lateinit var auth: FirebaseAuth
-    private lateinit var database: FirebaseDatabase
-    private lateinit var firstnameEditText: EditText
-    private lateinit var lastnameEditText: EditText
+    private lateinit var firstNameEditText: EditText
+    private lateinit var lastNameEditText: EditText
     private lateinit var emailEditText: EditText
+    private lateinit var usernameEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var confirmPasswordEditText: EditText
     private lateinit var registerButton: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_registerpage)
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
 
-        auth = FirebaseAuth.getInstance()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_registerpage, container, false)
+
+        mAuth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
 
-        firstnameEditText = findViewById(R.id.firstname)
-        lastnameEditText = findViewById(R.id.lastname)
-        emailEditText = findViewById(R.id.email)
-        passwordEditText = findViewById(R.id.password)
-        confirmPasswordEditText = findViewById(R.id.confirm_password)
-        registerButton = findViewById(R.id.enter_button)
+        firstNameEditText = view.findViewById(R.id.firstname)
+        lastNameEditText = view.findViewById(R.id.lastname)
+        emailEditText = view.findViewById(R.id.email)
+        usernameEditText = view.findViewById(R.id.username)
+        passwordEditText = view.findViewById(R.id.password)
+        confirmPasswordEditText = view.findViewById(R.id.confirm_password)
+        registerButton = view.findViewById(R.id.registerButton)
 
         registerButton.setOnClickListener {
-            val firstName = firstnameEditText.text.toString()
-            val lastName = lastnameEditText.text.toString()
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
-            val confirmPassword = confirmPasswordEditText.text.toString()
-
-            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
-                if (password == confirmPassword) {
-                    registerUser(firstName, lastName, email, password)
-                } else {
-                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
-            }
+            registerUser(
+                firstNameEditText.text.toString(),
+                lastNameEditText.text.toString(),
+                emailEditText.text.toString(),
+                usernameEditText.text.toString(),
+                passwordEditText.text.toString(),
+                confirmPasswordEditText.text.toString()
+            )
         }
+
+        return view
     }
 
-    private fun registerUser(firstName: String, lastName: String, email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
+    private fun registerUser(
+        firstName: String,
+        lastName: String,
+        email: String,
+        username: String,
+        password: String,
+        confirmPassword: String
+    ) {
+        if (password != confirmPassword) {
+            Toast.makeText(requireContext(), "Passwords do not match", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
-                    val userId = auth.currentUser?.uid ?: ""
-                    val userReference = database.reference.child("users").child(userId)
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = mAuth.currentUser
+                    val userProfileChangeRequest = UserProfileChangeRequest.Builder()
+                        .setDisplayName(username)
+                        .build()
+                    user?.updateProfile(userProfileChangeRequest)
 
-                    // Save user data to the database
-                    userReference.child("firstName").setValue(firstName)
-                    userReference.child("lastName").setValue(lastName)
-                    userReference.child("email").setValue(email)
+                    // Save additional user details to Firebase Realtime Database
+                    val userRef = database.reference.child("users").child(user?.uid ?: "")
+                    val userData = HashMap<String, String>()
+                    userData["firstName"] = firstName
+                    userData["lastName"] = lastName
+                    userData["email"] = email
+                    userData["username"] = username
+                    userRef.setValue(userData)
 
-                    Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
-                    // Example: startActivity(Intent(this, LoginActivity::class.java))
+                    // Navigate user to another screen
+                    startActivity(Intent(requireActivity(), LoginFragment::class.java))
                 } else {
-                    Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT).show()
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        requireContext(), "Registration failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
     }
