8c8533dfc7799a35bea9e02e985496c20b577b94
tony <tdnkvc@umsystem.edu>
2024-04-11 09:57:50 -0500

    fixed some issues with the laout and added user points to the rewards page

--- app/src/main/java/edu/missouri/collegerewards/ui/myrewards/MyRewardsViewModel.kt
+++ app/src/main/java/edu/missouri/collegerewards/ui/myrewards/MyRewardsViewModel.kt
@@ -3,11 +3,12 @@ package edu.missouri.collegerewards.ui.myrewards
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import edu.missouri.collegerewards.data.SingletonData
 
 class MyRewardsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "55 Points"
+        value = "${SingletonData.shared.currentUser.points} points"
     }
     val text: LiveData<String> = _text
 }
