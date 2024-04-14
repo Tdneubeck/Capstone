6fa5ea84082112db3754ca7caaf5626ee4ac10ec
tony <tdnkvc@umsystem.edu>
2024-04-14 13:57:50 -0500

    Fixed the leaderboard and added a default points to the registerpage

--- app/src/main/java/edu/missouri/collegerewards/ui/leaderboard/LeaderboardTile.kt
+++ app/src/main/java/edu/missouri/collegerewards/ui/leaderboard/LeaderboardTile.kt
@@ -1,6 +1,3 @@
 package edu.missouri.collegerewards.ui.leaderboard
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
 data class LeaderboardTile(val rank: Int, val username: String, val score: Int)
